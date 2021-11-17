c16206e001c230cd8d0787b4de53c0b7a986f590
Alexei2003 <91179353+Alexei2003@users.noreply.github.com>
2021-11-17 17:44:52 +0300

    567

--- src/test/java/zmeika/TutorialWindowTest.java
+++ src/test/java/zmeika/TutorialWindowTest.java
@@ -4,11 +4,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TutorialWindowTest {
+    private boolean flag = false;
     MainWindow MW = new MainWindow();
 
     @Test
     public void Window(){
-        Assert.assertNotNull(MW);
+        if (MW != null){
+            flag = true;
+        }
+        Assert.assertFalse(flag);
     }
 
 }
