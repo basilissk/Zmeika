312fad56a43eee0b6b14dd09c04ccdcd216049b8
Alexei2003 <91179353+Alexei2003@users.noreply.github.com>
2021-11-17 17:23:28 +0300

    6

--- src/main/java/zmeika/TutorialWindow.java
+++ src/main/java/zmeika/TutorialWindow.java
@@ -11,6 +11,5 @@ public class TutorialWindow extends JFrame {
         setBackground(Color.black);
         add(new Tutorial());
         setVisible(true);
-
     }
 }
--- src/test/java/zmeika/TutorialWindowTest.java
+++ src/test/java/zmeika/TutorialWindowTest.java
@@ -1,12 +1,12 @@
 package zmeika;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TutorialWindowTest {
-    TutorialWindow TW = new TutorialWindow();
-
     @Test
     public void Window(){
+        Assert.assertFalse(TutorialWindow.isDefaultLookAndFeelDecorated());
     }
 
 }
