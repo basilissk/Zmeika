d99b0a257e3fae6ad5fc5d35b336459a6b019fbf
Alexei2003 <91179353+Alexei2003@users.noreply.github.com>
2021-11-17 17:12:53 +0300

    4

--- src/test/java/zmeika/TutorialWindowTest.java
+++ src/test/java/zmeika/TutorialWindowTest.java
@@ -1,18 +1,13 @@
 package zmeika;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class TutorialWindowTest {
     private boolean flag;
-    zmeika.TutorialWindow TW = new zmeika.TutorialWindow();
 
     @Test
     public void Window(){
-        if (TW != null ){
-            flag = true;
-        }
-        Assert.assertTrue(flag);
+        TutorialWindow TW = new TutorialWindow();
     }
 
 }
