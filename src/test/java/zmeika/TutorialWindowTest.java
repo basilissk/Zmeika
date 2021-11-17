a6d83d57e6a9de577ae1051f143c312e18452d4f
Alexei2003 <91179353+Alexei2003@users.noreply.github.com>
2021-11-17 16:59:11 +0300

    1

---
+++ src/test/java/zmeika/TutorialWindowTest.java
@@ -0,0 +1,14 @@
+package zmeika;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TutorialWindowTest {
+    TutorialWindow TW = new TutorialWindow();
+
+    @Test
+    public void Window(){
+        Assert.assertNotNull(TW);
+    }
+
+}
